e78ad21363de4d4ad10242b1946cd34a91106d6f
agungaldi190701 <agung20.mhs@akb.ac.id>
2023-03-27 12:27:11 +0700

    mengetahui ukuran layar

--- lib/main.dart
+++ lib/main.dart
@@ -14,28 +14,61 @@ class MyWidget extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(),
-      home: MyHomePage(),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String message = 'Pesan Dari halaman pertama';
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    Orientation orientation = MediaQuery.of(context).orientation;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('First Screen'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => SecondScreen(message)));
-          },
-          child: const Text('Go to second screen'),
-        ),
+      backgroundColor: Colors.blueGrey,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            'Screen width : ${screenSize.width.toStringAsFixed(2)}',
+            style: TextStyle(color: Colors.white, fontSize: 18),
+            textAlign: TextAlign.center,
+          ),
+          Text(
+            'Orientation : ${orientation == Orientation.portrait ? 'Portrait' : 'Landscape'}',
+            style: TextStyle(color: Colors.white, fontSize: 18),
+            textAlign: TextAlign.center,
+          )
+        ],
       ),
     );
   }
 }
+
+
+// Second Page===============
+
+// class MyHomePage extends StatelessWidget {
+//   final String message = 'Pesan Dari halaman pertama';
+
+//   const MyHomePage({super.key});
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('First Screen'),
+//       ),
+//       body: Center(
+//         child: ElevatedButton(
+//           onPressed: () {
+//             Navigator.push(context,
+//                 MaterialPageRoute(builder: (context) => SecondScreen(message)));
+//           },
+//           child: const Text('Go to second screen'),
+//         ),
+//       ),
+//     );
+//   }
+// }

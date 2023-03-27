ceab7831feaacc803b27a87a0579f0c5bcefaff8
agungaldi190701 <agung20.mhs@akb.ac.id>
2023-03-27 11:49:04 +0700

    Listview.builder

--- lib/main.dart
+++ lib/main.dart
@@ -24,24 +24,22 @@ class ScrollingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: numberList.map((int number) {
-          return Container(
-            height: 250,
-            decoration: BoxDecoration(
-                color: Colors.grey, border: Border.all(color: Colors.black)),
-            child: Center(
-              child: Text(
-                '$number',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 50,
-                ),
+      body: ListView.builder(itemBuilder: (BuildContext context, int index) {
+        return Container(
+          height: 250,
+          decoration: BoxDecoration(
+              color: Colors.grey, border: Border.all(color: Colors.black)),
+          child: Center(
+            child: Text(
+              '${numberList[index]}',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 50,
               ),
             ),
-          );
-        }).toList(),
-      ),
+          ),
+        );
+      }),
     );
   }
 }

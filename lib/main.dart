2ad8c6470bc637af366efed3783ed85e22ff7e96
agungaldi190701 <agung20.mhs@akb.ac.id>
2023-03-27 12:02:41 +0700

    ExpendedFlexiblePage ()

--- lib/main.dart
+++ lib/main.dart
@@ -13,46 +13,56 @@ class MyWidget extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(),
-      home: Rainbow(),
+      home: ExpendedFlexiblePage(),
     );
   }
 }
 
-class Rainbow extends StatelessWidget {
+class ExpendedFlexiblePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Expanded(
-            child: Container(
-          color: Colors.red,
-        )),
-        Expanded(
-            child: Container(
-          color: Colors.orange,
-        )),
-        Expanded(
-            child: Container(
-          color: Colors.yellow,
-        )),
-        Expanded(
-            child: Container(
-          color: Colors.green,
-        )),
-        Expanded(
-            flex: 2,
-            child: Container(
-              color: Colors.blue,
-            )),
-        Expanded(
-            child: Container(
-          color: Colors.indigo,
-        )),
-        Expanded(
-            child: Container(
-          color: Colors.purple,
-        )),
-      ],
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        children: [
+          Row(
+            children: [
+              ExpendedWidget(),
+              FlexibleWidget(),
+            ],
+          ),
+          Row(
+            children: [
+              FlexibleWidget(),
+              ExpendedWidget(),
+            ],
+          ),
+        ],
+      )),
+    );
+  }
+}
+
+class ExpendedWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        color: Colors.red,
+        height: 100,
+      ),
+    );
+  }
+}
+
+class FlexibleWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      child: Container(
+        color: Colors.blue,
+        height: 100,
+      ),
     );
   }
 }
